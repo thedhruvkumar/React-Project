eb31728031b6c3be5d5191b0fea933e6bfadb509
thedhruvkumar <dhruv.buxar@gmail.com>
2022-04-11 14:10:29 +0530

    Update TextForm.jsx

--- src/Components/TextForm.jsx
+++ src/Components/TextForm.jsx
@@ -21,8 +21,8 @@ const TextForm = () => {
     <h1>Enter Text</h1>
   <textarea value={text} onChange={textChange} class="form-control" id="maintextForm" rows="3"></textarea>
 </div>
-<button className="btn btn-primary" onClick={handleUpper} >UpperCase</button>
-<button className="btn btn-primary" onClick={handleLower} >LowerCase</button>
+<button className="btn btn-primary m-1" onClick={handleUpper} >UpperCase</button>
+<button className="btn btn-primary m-1" onClick={handleLower} >LowerCase</button>
 {/* <button className="btn btn-primary" onClick={handleBtn} >UpperCase</button> */}
 {/* <button className="btn btn-primary" onClick={handleBtn} >UpperCase</button> */}
     </>
