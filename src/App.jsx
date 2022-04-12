11d585d23cce9bf3b16c4f70341cb34e9cc5ddc5
thedhruvkumar <dhruv.buxar@gmail.com>
2022-04-12 14:32:18 +0530

    Router

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,24 @@ import './App.css';
 import Footer from './Components/Footer';
 import NavBar from './Components/NavBar';
 import TextForm from './Components/TextForm';
+import {BrowserRouter , Route , Routes} from 'react-router-dom'
 
 function App() {
   return (
     <>
-    <div className="main">
-      <NavBar></NavBar>
-      <div className="container">
-      <TextForm></TextForm>
-      </div>
-      <Footer></Footer>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <>
+        <NavBar/><Footer/>
+          </>}>
+          <Route index element={<TextForm/>>} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
