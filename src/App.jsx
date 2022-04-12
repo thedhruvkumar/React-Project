c0f901e9ce03b0ac9c75a5a7e916a3324775dce9
thedhruvkumar <dhruv.buxar@gmail.com>
2022-04-12 14:35:25 +0530

    Updated Router

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './Components/Footer';
 import NavBar from './Components/NavBar';
 import TextForm from './Components/TextForm';
 import {BrowserRouter , Route , Routes} from 'react-router-dom'
+import Dictionary from './Components/Dictionary';
 
 function App() {
   return (
@@ -13,10 +14,10 @@ function App() {
           <>
         <NavBar/><Footer/>
           </>}>
-          <Route index element={<TextForm/>>} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
+          <Route index element={<TextForm/>} />
+          <Route path="blogs" element={<Footer/>} />
+          <Route path="contact" element={<Dictionary />} />
+          <Route path="*" element={<TextForm/>} />
         </Route>
       </Routes>
     </BrowserRouter>
