3779baa801ac7a5769c2cc23ea55c2281af03f95
thedhruvkumar <dhruv.buxar@gmail.com>
2022-04-12 14:03:54 +0530

    Update NavBar.jsx

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
   return (
       <>
      
-     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <div className="container-fluid">
     <a className="navbar-brand" href="#">Kinetics Lab</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
