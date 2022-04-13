d308fe4b8a784acde46f165902b2179ce038b361
thedhruvkumar <dhruv.buxar@gmail.com>
2022-04-13 20:44:01 +0530

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -11,20 +11,18 @@ function App() {
   return (
     <>
     
-    <NavBar></NavBar>
     <BrowserRouter>
+    <NavBar/>
       <Routes>
-       
+          <Route index element={<div className='container m-5 p-5'>Navigate through links</div>}/>
           <Route path="/blogs" element={<Footer/>} />
           <Route path="/dict" element={<Dictionary />} />
           <Route path="/text" element={<TextForm/>} />
           <Route path="/contact" element={<Contact/>} />
-      <Route path="/login" element={<Login></Login>}>
-        </Route>  
+      <Route path="/login" element={<Login></Login>}/>        
       </Routes>
-
+      <Footer/>
     </BrowserRouter>
-    <Footer></Footer>
     
     </>
   );
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,36 +1,49 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-      <>
-     
-     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div className="container-fluid">
-    <Link className="navbar-brand" to="#">Kinetics Lab</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link className="nav-Link active" aria-current="page" to="/">Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-Link" to="/text">Text Analyzer</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-Link" to="/dict">Dictionary</Link>
-        </li>
-        
-      </ul>
-    </div>
-  </div>
-</nav>
-      
-      </>
-  )
-}
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            Kinetics Lab
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-Link active" aria-current="page" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-Link" to="text">
+                  Text Analyzer
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-Link" to="dict">
+                  Dictionary
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
 
-export default NavBar
+export default NavBar;
