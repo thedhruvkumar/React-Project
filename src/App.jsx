388288d06bc7168210afc8c0b4ff56f941a712b6
thedhruvkumar <dhruv.buxar@gmail.com>
2022-04-13 20:06:29 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,27 @@ import TextForm from './Components/TextForm';
 import {BrowserRouter , Route , Routes} from 'react-router-dom'
 import Dictionary from './Components/Dictionary';
 import Contact from './Components/Contact';
+import Login from './Components/Login';
 
 function App() {
   return (
     <>
-    <NavBar></NavBar>
+    
+          <NavBar></NavBar>
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<TextForm></TextForm>}/>
+       
           <Route path="/blogs" element={<Footer/>} />
           <Route path="/dict" element={<Dictionary />} />
           <Route path="/text" element={<TextForm/>} />
           <Route path="/contact" element={<Contact/>} />
-        
+      <Route path="/login" element={<Login></Login>}>
+        </Route>  
       </Routes>
+
     </BrowserRouter>
-    <Footer></Footer>
+          <Footer></Footer>
+    
     </>
   );
 }
---
+++ src/Components/Login.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+    <div className="text-center">
+
+    <main className="container form-signin">
+  <form>
+    <img className="mb-4" src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"/>
+    <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+    <div className="form-floating">
+      <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
+      <label for="floatingInput">Email address</label>
+    </div>
+    <div className="form-floating">
+      <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
+      <label for="floatingPassword">Password</label>
+    </div>
+
+    <div className="checkbox mb-3">
+      <label>
+        <input type="checkbox" value="remember-me"/> Remember me
+      </label>
+    </div>
+    <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+    <p className="mt-5 mb-3 text-muted">© 2017–2021</p>
+  </form>
+</main>
+    </div>
+    </>
+  )
+}
+
+export default Login
