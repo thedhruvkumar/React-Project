2c33ae33bdc9fe2a1d534f93ae2c9fccecb20e24
thedhruvkumar <dhruv.buxar@gmail.com>
2022-04-14 11:34:34 +0530

    Update NavBar.jsx

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import { <NavLink></NavLink> } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
